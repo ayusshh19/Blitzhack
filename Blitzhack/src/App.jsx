59e42d9c504ac76e0d29fa72211542b37d8887ea
ayusshh19 <cm.b.49ayush.shukla@gmnail.com>
2025-07-07 22:30:02 +0530

    [FUNC] ROUTES CONFIGURED

--- Blitzhack/src/App.jsx
+++ Blitzhack/src/App.jsx
@@ -1,41 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { HoverCard, Button, Text, Group } from "@mantine/core";
-import { Table } from "@mantine/core";
-import { Title } from "@mantine/core";
+import Sidebar from "./component/Sidebar";
+import Dashboard from "./Pages/Dashboard";
+import Supplier from "./Pages/Supplier";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  const elements = [
-    { position: 6, mass: 12.011, symbol: "C", name: "Carbon" },
-    { position: 7, mass: 14.007, symbol: "N", name: "Nitrogen" },
-    { position: 39, mass: 88.906, symbol: "Y", name: "Yttrium" },
-    { position: 56, mass: 137.33, symbol: "Ba", name: "Barium" },
-    { position: 58, mass: 140.12, symbol: "Ce", name: "Cerium" },
-  ];
-
-  const rows = elements.map((element) => (
-    <Table.Tr key={element.name}>
-      <Table.Td>{element.position}</Table.Td>
-      <Table.Td>{element.name}</Table.Td>
-      <Table.Td>{element.symbol}</Table.Td>
-      <Table.Td>{element.mass}</Table.Td>
-    </Table.Tr>
-  ));
-
   return (
     <>
-      <Table>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th>Element position</Table.Th>
-            <Table.Th>Element name</Table.Th>
-            <Table.Th>Symbol</Table.Th>
-            <Table.Th>Atomic mass</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>{rows}</Table.Tbody>
-      </Table>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Sidebar />}>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/supplier" element={<Supplier />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
